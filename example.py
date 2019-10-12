5b11161bb5ce6ae214dcbe397356c84de4c337fb
Rahul-RB <rahulbharadwaj033@gmail.com>
2019-10-12 13:08:31 +0530

    Modified example.py as per setup.py

--- example.py
+++ example.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from src import CustomRotatingFileHandler as CRFH
-
+from RotatingTextFile import RotatingTextFile as rtf
 i=0
 def condition():
 	return i%10 == 0
 
-with CRFH("x.txt",condition,10) as fp:
+with rtf("tmp.log",condition,10) as fp:
 	while i<112:
 		fp.write(str(datetime.now().microsecond)+"\n")
 		i += 1
